5ac0628cbb39f5f9994dd66d0c04c11cc1c94e67
Nick Yu <yynickel@gmail.com>
2019-07-13 22:36:44 +0000

    step 11 done; allowed user to create review for a product

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,20 @@
 class ReviewsController < ApplicationController
   def create
-    raise "Yay, I'm here!"
+    @product=Product.find params[:product_id]
+    @review=@product.reviews.new(review_params)
+
+    if @review.save
+      redirect_to @product, notice: 'Review created!'
+    else
+      redirect_to :back, notice: 'Review cannot be created! Please check if you have filled the required fields'
+    end
+  end
+
+  def review_params
+    params.require(:review).permit(
+      :description,
+      :rating,
+      :user_id
+    )
   end
 end
