03b3c777f3a859aee84ed9b4c1bb2b00c5f2204d
Nick Yu <yynickel@gmail.com>
2019-07-15 21:14:22 +0000

    added validation tests for email and password format of the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   has_many :reviews
 
+  validates :first_name, :last_name, :email, :password, :password_confirmation,  presence:true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates_uniqueness_of :email, case_sensitive:false
   has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,25 +2,42 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'Validations' do
+    # test absence of required data fields
     it 'should not create user without a first name' do
-      user=User.create first_name:'John', last_name:'Doe', email:'a@a',password:'123'
-      expect(user).to be_valid
+      user=User.create last_name:'Doe', email:'a@a',password:'123', password_confirmation:'123'
+      expect(user).to_not be_valid
     end
     it 'should not create user without a last_name' do
-      user=User.create first_name:'John', last_name:'Doe', email:'a@a',password:'123'
-      expect(user).to be_valid
+      user=User.create first_name:'John', email:'a@a',password:'123', password_confirmation:'123'
+      expect(user).to_not be_valid
     end
     it 'should not create user without an email' do
-      user=User.create first_name:'John', last_name:'Doe', email:'a@a',password:'123'
-      expect(user).to be_valid
+      user=User.create first_name:'John', last_name:'Doe', password:'123', password_confirmation:'123'
+      expect(user).to_not be_valid
     end
-    it 'should not create user without a password' do
-      user=User.create first_name:'John', last_name:'Doe', email:'a@a',password:'123'
-      expect(user).to be_valid
+    it 'should not create user without a password or password confirmation' do
+      user=User.create first_name:'John', last_name:'Doe', email:'a@a'
+      expect(user).to_not be_valid
     end
 
-    it 'should create user with proper attribute and a unique email' do
-      user=User.create first_name:'John', last_name:'Doe', email:'a@a',password:'123'
+    #test password and email formatting
+    it 'should not create user without a matching password' do
+      user=User.create first_name:'John', last_name:'Doe', email:'a@a',password:'123', password_confirmation:'321'
+      expect(user).to_not be_valid
+    end
+    it 'should not create user with an incorrectly formatted email' do
+      user=User.create first_name:'John', last_name:'Doe', email:'abc',password:'123', password_confirmation:'123'
+      expect(user).to_not be_valid
+    end
+    it 'should not create user with an email that is already taken' do
+      user1=User.create first_name:'John', last_name:'Doe', email:'a@a',password:'123', password_confirmation:'123'
+      user2=User.create first_name:'John', last_name:'Doe', email:'A@A',password:'123', password_confirmation:'123'
+      expect(user2).to_not be_valid
+    end
+
+    # positive case
+    it 'should create user with proper attribute' do
+      user=User.create first_name:'John', last_name:'Doe', email:'a@a',password:'123',password_confirmation:'123'
       expect(user).to be_valid
     end
 
