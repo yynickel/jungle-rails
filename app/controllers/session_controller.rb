992cb440c9c41f1780d52b3d4c86c3a2c069ea1e
Nick Yu <yynickel@gmail.com>
2019-07-15 21:55:10 +0000

    changed session controller to utilize the user class authentication method

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,9 +4,9 @@ class SessionController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:user][:email])
+    user = User.authenticate_with_credentials(params[:user][:email],params[:user][:password])
     # If the user exists AND the password entered is correct.
-    if user && user.authenticate(params[:user][:password])
+    if user
       # Save the user id inside the browser cookie. This is how we keep the user
       # logged in when they navigate around our website.
       session[:user_id] = user.id
